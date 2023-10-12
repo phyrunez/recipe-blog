119ba1341c03c51d7a669e2768e673ecc461552a
Phyrunez <ebukajudee302@gmail.com>
2023-10-12 19:14:45 +0100

    worked on recipe blog

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+let addIngredientsBtn = document.getElementById('addIngredientsBtn');
+let ingredientList = document.querySelector('.ingredientList');
+let ingredientDiv = document.querySelectorAll('.ingredientDiv')[0];
+
+addIngredientsBtn.addEventListener('click', function(){
+    console.log('working')
+    let newIngredients = ingredientDiv.cloneNode(true);
+    let input = newIngredients.getElementsByTagName('input')[0];
+    input.value = '';
+    ingredientList.appendChild(newIngredients);
+})
